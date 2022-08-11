c2af7a08bf84a79b50b4282cf059ebac9edcb771
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-08-11 11:51:50 +0300

    fix: fix getAllUsers error

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { createUser, getUser, getAllUser } = require('../controllers/users');
+const { createUser, getUser, getAllUsers } = require('../controllers/users');
 
 router.post('/', createUser);
 router.get('/:id', getUser);
-router.get('/', getAllUser);
+router.get('/', getAllUsers);
 
 module.exports = router;
