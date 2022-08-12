c288bff0533c381d13da06dbb34d113725da7885
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-08-12 21:54:58 +0300

    feat: add delete/update user controllers & routes

--- controllers/users.js
+++ controllers/users.js
@@ -23,4 +23,26 @@ const getAllUsers = (req, res) => {
     .catch((err) => res.status(500).send({ message: `Error getting users ${err}` }));
 };
 
-module.exports = { createUser, getUserById, getAllUsers };
+const updateUserProfile = (req, res) => {
+  const { name, about } = req.body;
+  User
+    .findByIdAndUpdate(req.user._id, { name, about }, {
+      new: true, runValidators: true, upsert: true,
+    })
+    .then((user) => res.status(201).send({ data: user }))
+    .catch((err) => res.status(500).send({ message: `Error getting users ${err}` }));
+};
+
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User
+    .findByIdAndUpdate(req.user._id, { avatar }, {
+      new: true, runValidators: true, upsert: true,
+    })
+    .then((user) => res.status(201).send({ data: user }))
+    .catch((err) => res.status(500).send({ message: `Error getting users ${err}` }));
+};
+
+module.exports = {
+  createUser, getUserById, getAllUsers, updateUserProfile, updateUserAvatar,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const userRouter = require('express').Router();
-const { createUser, getUserById, getAllUsers } = require('../controllers/users');
+const {
+  createUser, getUserById, getAllUsers, updateUserProfile, updateUserAvatar,
+} = require('../controllers/users');
 
 userRouter.post('/', createUser);
 userRouter.get('/:userId', getUserById);
 userRouter.get('/', getAllUsers);
+userRouter.patch('/me', updateUserProfile);
+userRouter.patch('/me/avatar', updateUserAvatar);
 
 module.exports = userRouter;
