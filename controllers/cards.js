888869b441aa14439e0f2a6b449e92a77d553a8f
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-08-14 23:20:29 +0300

    feat: error handlers for cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,40 +1,114 @@
 const Card = require('../models/card');
+const BadRequestError = require('../errors/BadRequestError');
+const ServerError = require('../errors/ServerError');
+const NotFoundError = require('../errors/NotFoundError');
 
-const createCard = (req, res) => {
-  const { name, link } = req.body;
-  Card
-    .create({ name, link, owner: req.user._id })
-    .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(500).send({ message: `Error creating a card ${err}` }));
+const createCard = async (req, res) => {
+  try {
+    const { name, link } = req.body;
+    if (!link) {
+      throw new BadRequestError('Переданы некорректные данные при создании карточки: link');
+    }
+    if (!name) {
+      throw new BadRequestError('Переданы некорректные данные при создании карточки: name');
+    }
+    const card = await Card.create({ name, link, owner: req.user._id });
+    res.status(200).send(card);
+  } catch (err) {
+    if (err instanceof BadRequestError) {
+      res.status(err.codeStatus).send({ message: err.message });
+      return;
+    }
+    if (err) {
+      res.status(500).send({ message: `Ошибка сервера при создании карточки: ${err.message}` });
+      return;
+    }
+  }
 };
 
-const deleteCard = (req, res) => {
-  const { cardId } = req.params;
-  Card
-    .findByIdAndRemove(cardId)
-    .then((card) => res.status(201).send(card))
-    .catch((err) => res.status(500).send({ message: `Error deleting a card ${err}` }));
+const deleteCard = async (req, res) => {
+  try {
+    const card = await Card.findByIdAndRemove(req.params.cardId);
+    if (!card) {
+      throw new NotFoundError('Карточка с указанным _id не найдена.');
+    }
+    res.status(200).send(card);
+  } catch (err) {
+    if (err.name === 'NotFoundError') {
+      res.status(err.codeStatus).send({ message: err.message });
+      return;
+    }
+    if (err) {
+      res.status(500).send({ message: `Ошибка сервера при создании карточки: ${err.message}` });
+      return;
+    }
+  }
 };
 
-const getAllCards = (req, res) => {
-  Card
-    .find()
-    .then((cards) => res.status(201).send(cards))
-    .catch((err) => res.status(500).send({ message: `Error getting cards ${err}` }));
+const getAllCards = async (req, res) => {
+  try {
+    const cards = await Card.find();
+    res.status(200).send(cards);
+  } catch (err) {
+    if (err instanceof ServerError) {
+      res.status(err.codeStatus).send({ message: err.message });
+      return;
+    }
+    if (err) {
+      res.status(500).send({ message: `Ошибка сервера при получении карточек: ${err.message}` });
+      return;
+    }
+  }
 };
 
-const likeCard = (req, res) => {
-  Card
-    .findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .then((card) => res.status(201).send(card))
-    .catch((err) => res.status(500).send({ message: `Error liking card ${err}` }));
+const likeCard = async (req, res) => {
+  try {
+    const card = await Card.findByIdAndUpdate(req.params.cardId, {
+      $addToSet: { likes: req.user._id },
+    }, { new: true });
+    if (!card) {
+      throw new NotFoundError('Карточка с указанным _id не найдена.');
+    }
+    res.status(200).send(card);
+  } catch (err) {
+    if (err instanceof NotFoundError) {
+      res.status(err.codeStatus).send({ message: err.message });
+      return;
+    }
+    if (err instanceof BadRequestError) {
+      res.status(err.codeStatus).send({ message: err.message });
+      return;
+    }
+    if (err) {
+      res.status(500).send({ message: `Ошибка сервера при добавлении лайка карточке: ${err.message}` });
+      return;
+    }
+  }
 };
 
-const dislikeCard = (req, res) => {
-  Card
-    .findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .then((card) => res.status(201).send(card))
-    .catch((err) => res.status(500).send({ message: `Error disliking card ${err}` }));
+const dislikeCard = async (req, res) => {
+  try {
+    const card = await Card.findByIdAndUpdate(req.params.cardId, {
+      $pull: { likes: req.user._id },
+    }, { new: true });
+    if (!card) {
+      throw new NotFoundError('Карточка с указанным _id не найдена.');
+    }
+    res.status(200).send(card);
+  } catch (err) {
+    if (err instanceof NotFoundError) {
+      res.status(err.codeStatus).send({ message: err.message });
+      return;
+    }
+    if (err instanceof BadRequestError) {
+      res.status(err.codeStatus).send({ message: err.message });
+      return;
+    }
+    if (err) {
+      res.status(500).send({ message: `Ошибка сервера при удалении лайка карточки: ${err.message}` });
+      return;
+    }
+  }
 };
 
 module.exports = {
