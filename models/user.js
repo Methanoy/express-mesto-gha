3d478d7b43c692c76681bba208fa7ba4e9e8bd9b
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-08-18 21:06:53 +0300

    feat: add pass and email to the userSchema

--- models/user.js
+++ models/user.js
@@ -17,6 +17,17 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 30,
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
