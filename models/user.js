75d00b736a4786fbee8f78b2bcbbd494933e03e8
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-08-22 08:13:48 +0300

    feat: add findUserByCredentials func to the userSchema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -28,5 +29,21 @@ const userSchema = new mongoose.Schema({
     minlength: 4,
   },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильная почта или пароль.'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильная почта или пароль.'));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('User', userSchema);
