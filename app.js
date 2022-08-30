f08f7a122338a3c40fff73feaf5a9e1c54d93623
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-08-30 22:17:37 +0300

    feat: add celebrate to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
+const { celebrate, Joi, errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const cardsRouter = require('./routes/cards');
 const userRouter = require('./routes/users');
@@ -17,16 +18,30 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(helmet());
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(4),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required().pattern(/[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&/=]*)?/gi),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(4),
+  }),
+}), createUser);
 
 app.use('/users', auth, userRouter);
 app.use('/cards', auth, cardsRouter);
 app.use('*', (req, res) => res.status(NOT_FND_ERR_CODE).send({ message: 'Указан неправильный путь.' }));
 
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
