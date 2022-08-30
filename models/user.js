58f4d7289b6e62e3fac1c1c19d6b3ba3947f1423
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-08-30 22:51:30 +0300

    feat: add validation for email and tips for the rest fields to userSchema

--- models/user.js
+++ models/user.js
@@ -1,32 +1,35 @@
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Поле "Имя" должно содержать не менее 2 символов'],
+    maxlength: [30, 'Поле "Имя" должно содержать не более 30 символов'],
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Поле "О себе" должно содержать не менее 2 символов'],
+    maxlength: [30, 'Поле "О себе" должно содержать не более 30 символов'],
     default: 'Исследователь',
   },
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    match: /[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&/=]*)?/gi,
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
+    required: [true, 'Поле "Почта" является обязательным для заполнения'],
+    unique: [true, 'Пользователь с аналогичным email уже зарегистрирован'],
+    validate: [validator.isEmail, 'Поле "Почта" должно содержать email'],
   },
   password: {
     type: String,
-    required: true,
-    minlength: 4,
+    required: [true, 'Поле "Пароль" является обязательным для заполнения'],
+    minlength: [4, 'Поле "Пароль" должно содержать не менее 4 символов'],
     select: false,
   },
 });
