92c7ad0f0772e960ec97ce5feecbd279d17be499
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-08-31 09:04:30 +0300

    feat: add celebrate to the card route

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,15 @@
 const cardsRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   createCard, deleteCard, getAllCards, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-cardsRouter.post('/', createCard);
+cardsRouter.post('/', celebrate({
+  body: Joi.object.keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required(),
+  }),
+}), createCard);
 cardsRouter.get('/', getAllCards);
 cardsRouter.delete('/:cardId', deleteCard);
 cardsRouter.put('/:cardId/likes', likeCard);
