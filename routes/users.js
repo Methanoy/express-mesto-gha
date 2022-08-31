da3ad1797129e33b414f6f5198756f94432057b7
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-08-31 08:51:32 +0300

    feat: add celebrate to the user route

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const userRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUserById, getAllUsers, updateUserProfile, updateUserAvatar, getCurrentUserData,
 } = require('../controllers/users');
@@ -6,7 +7,16 @@ const {
 userRouter.get('/', getAllUsers);
 userRouter.get('/me', getCurrentUserData);
 userRouter.get('/:userId', getUserById);
-userRouter.patch('/me', updateUserProfile);
-userRouter.patch('/me/avatar', updateUserAvatar);
+userRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUserProfile);
+userRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(/[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&/=]*)?/i),
+  }),
+}), updateUserAvatar);
 
 module.exports = userRouter;
