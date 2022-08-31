17896d45bcf95f825b98c8998c9a8fb93e9d2f10
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-08-31 17:41:06 +0300

    ref: delete unnecessary auth check block in the auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,16 @@
 const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 // const { JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Требуется авторизация.' });
-  }
-
-  const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
 
   try {
     payload = jwt.verify(token, 'dev-secret');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Требуется авторизация.' });
+    throw new UnauthorizedError('Требуется авторизация.');
   }
 
   req.user = payload;
