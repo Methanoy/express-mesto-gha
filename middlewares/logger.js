61373af9527cc9f9e4d3344f6f4a7753683c110e
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-09-07 19:43:31 +0300

    feat: add requst logger

---
+++ middlewares/logger.js
@@ -0,0 +1,11 @@
+const winston = require('winston');
+const expressWinston = require('express-winston');
+
+const requestLogger = expressWinston.logger({
+  transports: [
+    new winston.transport.File({ filename: 'request.log' }),
+  ],
+  format: winston.format.json(),
+});
+
+module.exports = requestLogger;
