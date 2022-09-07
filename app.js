0ed0fbedcb3e6249c64c47686453ccfa0a1aefc6
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-09-07 20:19:47 +0300

    feat: mount logger to app

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const { createUser, login } = require('./controllers/users');
 const { validateLogin, validateCreateUser } = require('./middlewares/inputDataValidation');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -34,6 +35,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
+app.use(requestLogger);
 
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateCreateUser, createUser);
@@ -47,6 +49,7 @@ app.use(auth, () => {
   throw new NotFoundError('Указан неправильный путь.');
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
